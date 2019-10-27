23010b0f915073fbf577f2e684138b28edb625a4
Pedro Wanderley <peu-zico>
2019-10-27 16:55:01 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mustache = require('mustache-express');
+const router = require('./routes/index');
+const helpers = require('./helpers');
+const errorHandler = require('./handlers/errorHandler');
+
+// Configurações
+const app = express();
+
+app.use((req, res, next)=>{
+  res.locals.h = helpers;
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+app.use('/', router);
+
+app.use(errorHandler.notFound);
+
+app.engine('mst', mustache(__dirname+'/views/partials', '.mst'));
+app.set('view engine', 'mst');
+app.set('views', __dirname + '/views');
+
+module.exports = app;
---
+++ controllers/homeController.js
@@ -0,0 +1,8 @@
+exports.index = (req, res)=> {
+  let obj = {
+     pageTitle: "HOME",
+     userInfo: req.userInfo,
+  };
+ 
+  res.render('home', obj);
+}
