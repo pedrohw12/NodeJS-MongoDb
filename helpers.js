e518e7e2ee2a788f652bbd2e82a61b36073835e2
Pedro Wanderley <peu-zico>
2019-10-29 19:22:51 -0300

    Creating register to users

--- helpers.js
+++ helpers.js
@@ -3,5 +3,6 @@ exports.defaultPageTitle = "Site ABC";
 exports.menu = [
   {name: 'Home', slug: '/'},
   {name: 'Login', slug:'/users/login'},
+  {name: 'Cadastro', slug: '/users/register'},
   {name: 'Adicionar post', slug:'/post/add'},
 ];
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', homeController.index);  
 router.get('/users/login', userController.login);
 router.get('/users/register', userController.register);
+router.post('/users/register', userController.registerAction);
 
 router.get('/post/add', postController.add);
 router.post('/post/add', 
