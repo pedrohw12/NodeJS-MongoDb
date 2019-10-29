3bdfccdcf89fc0ee08f9c08dbce6f69dca3f6925
Pedro Wanderley <peu-zico>
2019-10-29 17:00:28 -0300

    Filtering tags

--- models/Post.js
+++ models/Post.js
@@ -33,4 +33,12 @@ postSchema.pre('save', async function(next) {
   next();
 });
 
+postSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind:'$tags' },
+    { $group:{ _id:'$tags', count:{ $sum:1 } } },
+    { $sort:{ count:-1 } }
+  ]);
+}
+
 module.exports = mongoose.model('Post', postSchema);
