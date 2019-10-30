867a7237bb7c07deffb7228d618b74fb5351254d
Pedro Wanderley <peu-zico>
2019-10-30 06:56:47 -0300

    Implementing password redefinition

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-module.exports.isLogged = (req, res, next) => {
+exports.isLogged = (req, res, next) => {
   if(!req.isAuthenticated()) {
     req.flash('error', 'Ops! Você não tem permissão para acessar essa página.');
     res.redirect('/users/login');
@@ -7,3 +7,18 @@ module.exports.isLogged = (req, res, next) => {
 
   next();
 };
+
+exports.changePassword = (req, res) => {
+  if (req.body.password != req.body['password-confirm']) {
+    req.flash('error', 'Senhas distintas');
+    res.redirect('/profile');
+    return;
+  }
+
+  req.user.setPassword(req.body.password, async ()=>{
+    await req.user.save();
+
+    req.flash('success', 'Senha alterada com sucesso!');
+    res.redirect('/');
+  });
+}; 
